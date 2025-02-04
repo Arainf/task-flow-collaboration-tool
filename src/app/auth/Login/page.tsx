1d34ccda5f48c07f3145ccdff4e68c1b9465c63f
Arainf <adrianrainierf@gmail.com>
2025-02-05 00:10:39 +0800

    Add initial Login component structure

---
+++ src/app/auth/Login/page.tsx
@@ -0,0 +1,18 @@
+
+export default function Login() {
+    return (
+        <>
+            <div className="flex flex-row w-screen h-screen">
+
+                <div className="">
+
+                </div>
+                
+                <div className="">
+
+                </div>
+                
+            </div>
+        </>
+    )
+}
