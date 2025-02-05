e4876b6b46187c3a92ae2f56c976f5b4d958012d
Arainf <adrianrainierf@gmail.com>
2025-02-05 23:50:50 +0800

    Add react-xarrows; update Login component with draggable elements and arrows

--- src/app/auth/Login/page.tsx
+++ src/app/auth/Login/page.tsx
@@ -5,16 +5,22 @@ import FacebookLogo from "@/assets/svg/facebook-logo.svg"
 import GoogleLogo from "@/assets/svg/google-logo.svg"
 import Peepsitting from "@/assets/svg/peep-siting.svg"
 import Samplechart from "@/assets/svg/sample-chart.svg"
+import Profileone from "@/assets/svg/profile-1.svg"
+import Profiletwo from "@/assets/svg/profile-2.svg"
 
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import Xarrow, {useXarrow, Xwrapper} from 'react-xarrows';
 
 import * as motion from "motion/react-client"
 import { useState, useRef } from "react"
 
 
 export default function Login() {
+    const [position, setPosition] = useState({ x: -150, y: -290 });
     const containerRef = useRef<HTMLDivElement>(null);
+    const headerboxRef = useRef<HTMLDivElement>(null);
+    const updateArrow = useXarrow();
 
     
     
@@ -32,8 +38,8 @@ export default function Login() {
 
                     <form className="flex flex-col gap-y-6 my-10 px-20 items-center">
                         <div className="flex flex-col gap-y-4 w-full   ">
-                            <Input type="text" placeholder="Username" className="h-[2.8rem] !placeholder:text-white rounded-full border-2 my-1 border-black" />
-                            <Input type="password" placeholder="Password"  className="h-[2.8rem] !placeholder:text-white rounded-full border-2 my-1 border-black"/>
+                            <Input type="text" placeholder="Username" className="h-[2.8rem] placeholder:text-black placeholder:font-semibold rounded-full border-2 my-1 border-black" />
+                            <Input type="password" placeholder="Password"  className="h-[2.8rem] placeholder:text-black placeholder:font-semibold rounded-full border-2 my-1 border-black"/>
                         </div>
                          <a href="" className="self-end ">forgot password?</a>
                         <Button type="submit" className="rounded-full font-bold w-full h-[2.8rem]">Login</Button>
@@ -60,19 +66,35 @@ export default function Login() {
                 </div>
                 
                 <div ref={containerRef} className="flex flex-col items-center w-3/5 h-full flex-1 border-black border-4 rounded-[25]  bg-[#ADEBFF51] @container">
-                    <div className="flex flex-col w-3/5 h-auto items-center  text-center p-8">
+                    
+                    <div ref={headerboxRef} className="flex flex-col w-3/5 h-auto items-center  text-center p-8">
                         <h1 className="lg:max-xl:text-1xl xl:max-2xl:text-2xl 2xl:text-3xl  "><b>Collaborate</b>, <b>Communicate</b>, and <b>Conquer</b> Tasks Together!</h1>
                         <h1 className="lg:max-xl:text-lg xl:max-2xl:text-xl 2xl:text-2xl font-bold"><i>- TaskFlow Developers</i></h1>
                     </div>
                     <div className="flex w-full h-full justify-end items-end">
                         <Image className="flex fixed bottom-[-3]" src={Peepsitting} alt="Peep Siting" width={300} height={300} />
+                        <div id="person-hand" className="bg-white -z-10 h-1 w-1 fixed bottom-[168px] right-[289px] "></div>
                     </div>
+                    <Xarrow
+                        startAnchor="bottom"
+                        showHead={false}
+                        lineColor="#121212"
+                        start={headerboxRef}
+                        end="draggable-box-1"
+                        endAnchor="top"
+                        strokeWidth={3}
+                    />
                     <motion.div 
-                    className="w-[316px] h-[202px] flex flex-col items-center p-6 bg-white rounded-[50px] shadow-[4px_8px_13px_rgba(0,0,0,0.25)] border-4 border-black"
+                    className=" w-[316px] h-[202px] flex flex-col items-center p-6 bg-white rounded-[50px] shadow-[4px_8px_13px_rgba(0,0,0,0.25)] border-4 border-black"
                     drag 
                     dragConstraints={containerRef}
                     dragElastic={0.2}
+                    onDrag={updateArrow}
+                    style={{ x: position.x, y: position.y }}
+                    id="draggable-box-1"
                     >
+                        <Image className="absolute top-[-33%] right-[-18%]" src={Profileone} alt="" width={60} height={60}/>
+                        <Image className="absolute bottom-[-33%] left-[-18%]" src={Profiletwo} alt="" width={60} height={60}/>
                         <span className="text-3xl font-semibold">Event Planning</span>
                         <span className="self-start pl-6">+ 10 Tasks</span>
                         <div className="flex flex-row justify-between w-full">
@@ -81,11 +103,22 @@ export default function Login() {
                             </div>
                             <div className="flex pr-8">
                                 <Image src={Samplechart} alt="Sample Chart" width={70} height={50}/>
+                                
                             </div>
                             
                         </div>
-                        
                     </motion.div>
+                    <Xarrow
+                    strokeWidth={3}
+                        showHead={false}
+                        lineColor="#121212"
+                        start="draggable-box-1"
+                        startAnchor="bottom"
+                        end="person-hand"
+                        endAnchor="left"
+                    />
+
+                    <span className="futura-bold self-start pl-4">@ 2025 ProjectTaskFlow</span>
                 </div>
                 
             </div>
